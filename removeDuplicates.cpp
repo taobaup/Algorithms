4ab88b7d13d89dc8316ba2789b5bf5a11621d2b8
taobaup <2970749159@qq.com>
2016-10-17 23:30:35 +0800

    Update removeDuplicates.cpp

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -3,22 +3,21 @@
 //若数组非空，从1~size-1遍历数组，依次与新数组的最后一个元素比较即可
 class Solution {
 public:
-	int removeDuplicates(vector<int>& nums) 
-	{
-		const int size=nums.size();
-		if(size==0)
-			return 0;
-
-		int index=0;
-		for(int i=1;i<size;++i)
-		{
-			if(nums[i]!=nums[index])
-			{
-				++index;
-				nums[index]=nums[i];
-			}
-		}
-
-		return index+1;
-	}
+    int removeDuplicates(vector<int>& nums) {
+        const int size=nums.size();
+        if(size==0)
+            return 0;
+        
+        int index=0;
+        for(int i=1;i<size;++i)
+        {
+            if(nums[i]!=nums[index])
+            {
+                ++index;
+                nums[index]=nums[i];
+            }
+        }
+        
+        return index+1;
+    }
 };
