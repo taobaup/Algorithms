54a817e12c98e0632c824375b881684c74fb5d20
taobaup <2970749159@qq.com>
2016-10-31 20:59:49 +0800

    Update and rename kthToLast.cpp to nthToLast.cpp

--- nthToLast.cpp
+++ nthToLast.cpp
@@ -1,12 +1,12 @@
-//查找链表中的倒数第k个结点
-//给定单链表和k，找到单链表倒数第k个节点
+//查找链表中的倒数第n个结点
+//给定单链表和n，找到单链表倒数第n个节点
 //如2->6->8->10->null，3则输出6
-//利用快慢指针，快指针先向前走k步
+//利用快慢指针，快指针先向前走n步
 //接着，快慢指针同时出发
 //若快指针指向倒数第1结点
-//则慢指针指向倒数第k结点
-//注意判断头结点的有效性，k值是否为0
-//以及快指针是否还没达到第k步就已遍历完链表
+//则慢指针指向倒数第n结点
+//注意判断头结点的有效性，n值是否为0
+//以及快指针是否还没达到第n步就已遍历完链表
 /**
  * Definition of ListNode
  * class ListNode {
@@ -21,12 +21,12 @@
  */
 class Solution {
 public:
-    ListNode *kthToLast(ListNode *head, int k) {
-        if(head==NULL||k==0)
+    ListNode *nthToLast(ListNode *head, int n) {
+        if(head==NULL||n==0)
             return NULL;
         
         ListNode *fast=head,*slow=head;
-        for(int i=0;i<k-1;++i)
+        for(int i=0;i<n-1;++i)
         {
             fast=fast->next;
             if(fast==NULL)
