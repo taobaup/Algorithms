2b7eb5be286d74db71fa003acb646d5d4150df40
taobaup <2970749159@qq.com>
2016-11-10 20:59:30 +0800

    Create Insert Interval.cpp

---
+++ Insert Interval.cpp
@@ -0,0 +1,40 @@
+//Insert Interval 
+//若newInterval.end < it->start，则插入it之前 
+//若newInterval.start > it->end，则继续遍历 
+//否则，取min左跟max右取代原先间隔
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+	vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+		auto it = intervals.begin();
+
+		while (it != intervals.end())
+		{
+			if (newInterval.end < it->start)
+			{
+				intervals.insert(it, newInterval);
+				return intervals;
+			}
+			else if (newInterval.start > it->end)
+				++it;
+			else
+			{
+				newInterval.start = min(newInterval.start, it->start);
+				newInterval.end = max(newInterval.end, it->end);
+				it = intervals.erase(it);
+			}
+		}
+
+		intervals.insert(intervals.end(), newInterval);
+
+		return intervals;
+	}
+};
