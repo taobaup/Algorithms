fb8ea49987e563169099ac4d13179ac93ab4f82b
taobaup <2970749159@qq.com>
2016-11-10 18:30:15 +0800

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -4,14 +4,14 @@
 class Solution {
 public:
     int reverse(int x) {
-	    int result = 0;
+	int result = 0;
 
-	    while (x)
-	    {
-		    result = result * 10 + x % 10;
-		    x = x / 10;
-	    }
+	while (x)
+	{
+		result = result * 10 + x % 10;
+		x = x / 10;
+	}
 
-	    return result;
+	return result;
     }
 };
