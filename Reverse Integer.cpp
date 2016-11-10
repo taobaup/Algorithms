97ec0974091ec3af42aa4d2ff3920f7570b78379
taobaup <2970749159@qq.com>
2016-11-10 19:10:10 +0800

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -1,17 +1,27 @@
 //Reverse Integer 
 //取模，依次进位即可 
+//为防止result * 10 + num % 10 > INT_MAX越界
+//应判断result > (INT_MAX - num % 10) / 10
 //时间复杂度 O(logn)，空间复杂度 O(1)
 class Solution {
 public:
-    int reverse(int x) {
-	int result = 0;
-
-	while (x)
+	int reverse(int x)
 	{
-		result = result * 10 + x % 10;
-		x = x / 10;
-	}
+		if (x == INT_MIN)
+			return 0;
 
-	return result;
-    }
+		int num = abs(x);
+		int result = 0;
+
+		while (num)
+		{
+			if (result > (INT_MAX - num % 10) / 10)
+				return 0;
+
+			result = result * 10 + num % 10;
+			num = num / 10;
+		}
+
+		return x > 0 ? result : -result;
+	}
 };
