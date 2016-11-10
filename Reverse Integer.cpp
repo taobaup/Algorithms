99eb440d1558310ce122b9fbe3d9338242e0b709
taobaup <2970749159@qq.com>
2016-11-10 18:27:29 +0800

    Create Reverse Integer.cpp

---
+++ Reverse Integer.cpp
@@ -0,0 +1,17 @@
+//Reverse Integer 
+//取模，依次进位即可 
+//时间复杂度 O(logn)，空间复杂度 O(1)
+class Solution {
+public:
+    int reverse(int x) {
+	    int result = 0;
+
+	    while (x)
+	    {
+		    result = result * 10 + x % 10;
+		    x = x / 10;
+	    }
+
+	    return result;
+    }
+};
