da78e48e40d8bb421e72e70c10f2065046eb6b11
taobaup <2970749159@qq.com>
2016-12-08 23:51:13 +0800

    Create ReplaceSpace.cpp

---
+++ ReplaceSpace.cpp
@@ -0,0 +1,42 @@
+解题思路：
+计算原字符串长度和空格长度，得到新字符串长度
+判断是否越界，若不越界，从后往前复制，遇到空格替换成“%20”
+之所以从后往前复制是因为从前往后复制需要多次移动字符，从后往前复制可以减少移动的次数
+class Solution {
+public:
+	void replaceSpace(char *str, int length) {
+		if (str == NULL || length < 0)
+			return;
+
+		int origin = 0;
+		int space = 0;
+		for (int i = 0; str[i] != '\0'; ++i)
+		{
+			++origin;
+			if (str[i] == ' ')
+				++space;
+		}
+
+		int newLength = origin + 2 * space;
+		int indexOfOrigin = origin;
+		int indexOfNew = newLength;
+
+		if (newLength > length)
+			return;
+
+		while (indexOfOrigin >= 0 && indexOfNew > indexOfOrigin)
+		{
+			if (str[indexOfOrigin] == ' ')
+			{
+				str[indexOfNew--] = '0';
+				str[indexOfNew--] = '2';
+				str[indexOfNew--] = '%';
+			}
+			else
+				str[indexOfNew--] = str[indexOfOrigin];
+			--indexOfOrigin;
+		}
+
+		return;
+	}
+};
