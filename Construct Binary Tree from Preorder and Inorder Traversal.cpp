5af94077eb20035d7cb73863b515bdbfa05e6541
taobaup <2970749159@qq.com>
2016-12-10 23:37:18 +0800

    Create Construct Binary Tree from Preorder and Inorder Traversal.cpp

---
+++ Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,42 @@
+解题思路：取出前序遍历序列第一个结点的值，即根结点的值
+在中序遍历序列中找出根结点的位置，计算distance(in_first, in_left_last)为len
+则pre_left_last = next(pre_first, len + 1)
+前序遍历序列中对应的左子树的前序遍历序列为next(pre_first), pre_left_last
+中序遍历序列中对应的左子树的中序遍历序列为in_first, in_left_last
+前序遍历序列中对应的右子树的前序遍历序列为pre_left_last, pre_last
+前序遍历序列中对应的右子树的前序遍历序列为next(in_left_last), in_last
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		return buildTree(begin(preorder), end(preorder), begin(inorder), end(inorder));
+	}
+
+	template<typename MyIt>
+	TreeNode* buildTree(MyIt pre_first,MyIt pre_last,MyIt in_first,MyIt in_last) {
+		if (pre_first == pre_last)
+			return nullptr;
+		if (in_first == in_last)
+			return nullptr;
+
+		const int rootValue = *pre_first;
+		auto in_left_last = find(in_first, in_last, rootValue);
+		int len = distance(in_first, in_left_last);
+		auto pre_left_last = next(pre_first, len + 1);
+
+		TreeNode *root = new TreeNode(rootValue);
+		root->left = buildTree(next(pre_first), pre_left_last, in_first, in_left_last);
+		root->right = buildTree(pre_left_last, pre_last, next(in_left_last), in_last);
+
+		return root;
+	}
+
+};
