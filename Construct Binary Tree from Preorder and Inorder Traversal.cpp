7fda6cefda0f5e5aaad1712a2578dc5e4ab93df4
taobaup <2970749159@qq.com>
2016-12-11 00:18:24 +0800

    Update Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -6,35 +6,35 @@
 前序遍历序列中对应的右子树的前序遍历序列为pre_left_last, pre_last
 前序遍历序列中对应的右子树的前序遍历序列为next(in_left_last), in_last
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
 class Solution {
 public:
-	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-		return buildTree(begin(preorder), end(preorder), begin(inorder), end(inorder));
+	TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+		return buildTree(begin(inorder), end(inorder), begin(postorder), end(postorder));
 	}
 
 	template<typename MyIt>
-	TreeNode* buildTree(MyIt pre_first,MyIt pre_last,MyIt in_first,MyIt in_last) {
-		if (pre_first == pre_last)
-			return nullptr;
+	TreeNode* buildTree(MyIt in_first, MyIt in_last, MyIt post_first, MyIt post_last) {
 		if (in_first == in_last)
 			return nullptr;
+		if (post_first == post_last)
+			return nullptr;
 
-		const int rootValue = *pre_first;
+		const int rootValue = *prev(post_last);
 		auto in_left_last = find(in_first, in_last, rootValue);
 		int len = distance(in_first, in_left_last);
-		auto pre_left_last = next(pre_first, len + 1);
+		auto post_left_last = next(post_first, len);
 
 		TreeNode *root = new TreeNode(rootValue);
-		root->left = buildTree(next(pre_first), pre_left_last, in_first, in_left_last);
-		root->right = buildTree(pre_left_last, pre_last, next(in_left_last), in_last);
+		root->left = buildTree(in_first, in_left_last, post_first, post_left_last);
+		root->right = buildTree(next(in_left_last), in_last, post_left_last, prev(post_last));
 
 		return root;
 	}
