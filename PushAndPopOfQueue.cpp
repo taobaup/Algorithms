1eef4150d405e15e2dac9937b2726de26caf59fb
taobaup <2970749159@qq.com>
2016-12-12 23:22:19 +0800

    Create PushAndPopOfQueue.cpp

---
+++ PushAndPopOfQueue.cpp
@@ -0,0 +1,33 @@
+解题思路：
+入队：直接push进stack1
+出队：判断stack2是否为空
+如果为空，则将stack1中所有元素pop，并push进栈stack2，取出stack2的栈顶;
+如果不为空，直接取出stack2的栈顶
+class Solution
+{
+public:
+	void push(int node) {
+		stack1.push(node);
+	}
+
+	int pop() {
+		if (stack2.empty())
+		{
+			while (!stack1.empty())
+			{
+				int &temp = stack1.top();
+				stack1.pop();
+				stack2.push(temp);
+			}
+		}
+
+		int result = stack2.top();
+		stack2.pop();
+
+		return result;
+	}
+
+private:
+	stack<int> stack1;
+	stack<int> stack2;
+};
