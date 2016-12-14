02a5c69dd0a9e6bdf78404f22c674ddac5451f49
taobaup <2970749159@qq.com>
2016-12-14 12:20:38 +0800

    Update Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,3 +1,10 @@
+题目：Construct Binary Tree from Preorder and Inorder Traversal 
+即：前/中序遍历序列重建二叉树
+
+题意：输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+
 解题思路：取出前序遍历序列第一个结点的值，即根结点的值
 在中序遍历序列中找出根结点的位置，计算distance(in_first, in_left_last)为len
 则pre_left_last = next(pre_first, len + 1)
@@ -6,6 +13,7 @@
 前序遍历序列中对应的右子树的前序遍历序列为pre_left_last, pre_last
 中序遍历序列中对应的右子树的中序遍历序列为next(in_left_last), in_last
 然后递归处理
+
 /**
 * Definition for a binary tree node.
 * struct TreeNode {
