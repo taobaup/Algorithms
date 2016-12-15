606115d23c52f8a406b832dbc6f28a79e2dd160d
taobaup <2970749159@qq.com>
2016-12-16 00:20:22 +0800

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -15,10 +15,13 @@ public:
 	int Fibonacci(int n) {
 		if (n < 0)
 			return -1;
+
 		if (n == 0)
 			return 0;
+
 		if (n == 1)
 			return 1;
+
 		else
 			return Fibonacci(n - 1) + Fibonacci(n - 2);
 	}
@@ -30,8 +33,10 @@ public:
 	int Fibonacci(int n) {
 		if (n < 0)
 			return -1;
+
 		if (n == 0)
 			return 0;
+
 		if (n == 1)
 			return 1;
 
