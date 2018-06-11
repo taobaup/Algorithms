300906433973a1bf99140e3df1b851a4670ef16a
taobaup <2970749159@qq.com>
2018-06-11 18:49:58 +0800

    Create deleteDuplicates.cpp

---
+++ LeetCode/Algorithms/083. Remove Duplicates from Sorted List/deleteDuplicates.cpp
@@ -0,0 +1,27 @@
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+        	return head;
+
+        ListNode *start = head;
+        while(start != NULL)
+        {
+        	ListNode *cur = start;
+        	while(cur->next != NULL && cur->next->val == cur->val)
+        		cur = cur->next;
+
+        	start->next = cur->next;
+        	start = start->next;
+        }
+
+        return head;
+    }
+};
