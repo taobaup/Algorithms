3fb9b440c9e5521bfdfd80732b41afb06885ad9c
taobaup <2970749159@qq.com>
2018-06-21 11:22:10 +0800

    Update searchInsert.cpp

--- LeetCode/Algorithms/035. Search Insert Position/searchInsert.cpp
+++ LeetCode/Algorithms/035. Search Insert Position/searchInsert.cpp
@@ -1,3 +1,4 @@
+1）
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -5,19 +6,56 @@ public:
         if(len <= 0)
         	return 0;
 
-        int left = 0;
-        int right = len - 1;
-        while(left <= right)
+        if(target == nums[len - 1])
+        	return len - 1;
+        else if(target > nums[len - 1])
+        	return len;
+
+        // i < len 而不是 len -1
+        for(int i = 0; i < len; ++i)
         {
-        	int mid = left + ((right - left) >> 1);
-        	if(target > nums[mid])
-        		left = mid + 1;
-        	else if(target < nums[mid])
-        		right = mid - 1;
-        	else
-        		return mid;
+        	// 注意不要把条件写反
+        	if(nums[i] >= target)
+        		return i;
         }
 
-        return left;
+        // 注意不要写成 return -1;
+        return len;
+    }
+};
+
+
+
+
+2）
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+    	int len = nums.size();
+
+    	if(len <= 0)
+    		return 0;
+
+    	if(target <= nums[0])
+    		return 0;
+    	else if(target == nums[len - 1])
+    		return len - 1;
+    	else if(target > nums[len - 1])
+    		return len;
+
+    	int left = 0;
+    	int right = len - 1;
+    	while(left <= right)
+    	{
+    		int mid = left + ((right - left) >> 1);
+    		if(target == nums[mid])
+    			return mid;
+    		else if(target > nums[mid])
+    			left = mid + 1;
+    		else
+    			right = mid - 1;
+    	}
+
+    	return left;
     }
 };
