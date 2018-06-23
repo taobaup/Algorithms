be96d32a5f5da1c402ff420b83702ce08c96c1c1
taobaup <2970749159@qq.com>
2018-06-23 15:39:33 +0800

    Create search.cpp

---
+++ LeetCode/Algorithms/081. Search in Rotated Sorted Array II/search.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        const int len = nums.size();
+        if(len <= 0)
+        	return 0;
+
+        int left = 0;
+        int right = len - 1;
+        while(left <= right)
+        {
+        	int mid = left + ((right - left) >> 1);
+        	if(target == nums[mid])
+        		return 1;
+
+        	if(nums[left] < nums[mid])
+        	{
+        		if(nums[left] <= target && target < nums[mid])
+        			right = mid - 1;
+        		else
+        			left = mid + 1;
+        	}
+        	else if(nums[left] > nums[mid])
+        	{
+        		if(nums[mid] < target && target <= nums[right])
+        			left = mid + 1;
+        		else
+        			right = mid - 1;
+        	}
+        	else
+        		++left;
+        }
+
+        return 0;
+    }
+};
