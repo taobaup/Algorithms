0e82de8de4cce72c8acf00b636f9d01f4ed6b4e9
taobaup <2970749159@qq.com>
2018-06-24 17:20:19 +0800

    Create FindNumbersWithSum.cpp

---
+++ nowcoder/coding-interviews/FindNumbersWithSum.cpp
@@ -0,0 +1,33 @@
+// 和为S的两个数字
+// 左右夹逼，因为 (a+1)(b-1) > ab
+// 所以最先找到的两数乘积必然最小
+// 注意返回的是两数字而不是小标
+
+
+class Solution {
+public:
+    vector<int> FindNumbersWithSum(vector<int> array,int sum) {
+    	vector<int> result;
+        int len = array.size();
+        if(len <= 1)
+        	return result;
+
+        int i = 0;
+        int j = len - 1;
+        while(i < j)
+        {
+        	if(array[i] + array[j] == sum)
+        	{
+        		result.push_back(array[i]);
+        		result.push_back(array[j]);
+        		break;
+        	}
+        	else if(array[i] + array[j] < sum)
+        		++i;
+        	else
+        		--j;
+        }
+
+        return result;
+    }
+};
