5b15639929fd157bd14705133d01c272ce304f67
taobaup <2970749159@qq.com>
2018-06-24 15:16:34 +0800

    Create twoSum.cpp

---
+++ LeetCode/Algorithms/001. Two Sum/twoSum.cpp
@@ -0,0 +1,56 @@
+1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        int len = nums.size();
+        if(len <= 1)
+        	return result;
+
+        unordered_map<int, int> map;
+        for(int i = 0; i < len; ++i)
+        	map[nums[i]] = i;
+
+        for(int i = 0; i < len; ++i)
+        {
+        	int temp = target - nums[i];
+        	if(map.find(temp) != map.end() && map[temp] > i)
+        	{
+        		result.push_back(i);
+        		result.push_back(map[temp]);
+        		break;
+        	}
+        }
+
+        return result;
+    }
+};
+
+
+
+
+2)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        int len = nums.size();
+        if(len <= 1)
+        	return result;
+
+        unordered_map<int, int> map;
+        for(int i = 0; i < len; ++i)
+        {
+        	int temp = target - nums[i];
+        	if(map.find(temp) != map.end())
+        	{
+        		result.push_back(map[temp]);
+        		result.push_back(i);
+        		return result;
+        	}
+        	map[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
