55632b15b975f83a52be2e8428f2bb8ede4c0402
taobaup <2970749159@qq.com>
2018-07-05 18:44:27 +0800

    Update max_subarray.cpp

--- Introduction_to_Algorithms/ch04/max_subarray.cpp
+++ Introduction_to_Algorithms/ch04/max_subarray.cpp
@@ -34,3 +34,79 @@ int main()
 
 	return 0;
 }
+
+
+
+
+2)
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int find_max_crossing_subarray(vector<int> &nums, int left, int mid, int right)
+{
+	int left_sum = INT_MIN;
+	int sum = 0;
+	
+	for (int i = mid; i >= left; --i)
+	{
+		sum += nums[i];
+		if (sum > left_sum)
+		{
+			left_sum = sum;
+		}
+	}
+
+	int right_sum = INT_MIN;
+	sum = 0;
+	for (int j = mid + 1; j <= right; ++j)
+	{
+		sum += nums[j];
+		if (sum > right_sum)
+		{
+			right_sum = sum;
+		}
+	}
+
+	return left_sum + right_sum;
+}
+
+int find_maximum_subarray(vector<int> &nums, int left, int right)
+{
+	int left_sum = INT_MIN;
+	int right_sum = INT_MIN;
+	int cross_sum = INT_MIN;
+
+	if (left == right)
+		return nums[left];
+	else
+	{
+		int mid = left + (right - left) / 2;
+		left_sum = find_maximum_subarray(nums, left, mid);
+		right_sum = find_maximum_subarray(nums, mid + 1, right);
+		cross_sum = find_max_crossing_subarray(nums, left, mid, right);
+
+		if (left_sum >= cross_sum)
+			return left_sum;
+		else if (right_sum >= cross_sum)
+			return right_sum;
+		else
+			return cross_sum;
+	}
+}
+
+int main()
+{
+	vector<int> nums = { 1, -2, 3, 10, -4, 7, 2, -5 };
+	int len = nums.size();
+	if (len <= 0)
+		return 0;
+	else
+	{
+		int res = find_maximum_subarray(nums, 0, len - 1);
+		cout << res << endl;
+	}
+
+	return 0;
+}
