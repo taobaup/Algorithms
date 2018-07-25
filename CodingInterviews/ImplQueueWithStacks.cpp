c02d61966452dccdbfc3e565d0cf519d6ec458bf
taobaup <2970749159@qq.com>
2018-07-25 11:37:31 +0800

    Create ImplQueueWithStacks.cpp

---
+++ CodingInterviews/ImplQueueWithStacks.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+        if(stack2.empty())
+        {
+        	while(!stack1.empty())
+        	{
+        		int &temp = stack1.top();
+        		stack1.pop();
+        		stack2.push(temp);
+        	}
+        }
+
+        int &res = stack2.top();
+        stack2.pop();
+
+        return res;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
