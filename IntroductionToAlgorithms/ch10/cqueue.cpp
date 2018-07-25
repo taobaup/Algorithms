541af1059c891e3191402de15ba667b30e357ebd
taobaup <2970749159@qq.com>
2018-07-25 11:25:59 +0800

    Update cqueue.cpp

--- IntroductionToAlgorithms/ch10/cqueue.cpp
+++ IntroductionToAlgorithms/ch10/cqueue.cpp
@@ -1,3 +1,26 @@
+stack	member functions:
+
+stack::emplace
+stack::empty
+stack::pop
+stack::push
+stack::size
+stack::swap
+stack::top
+
+
+queue	member functions:
+
+queue::emplace
+queue::empty
+queue::pop
+queue::push
+queue::size
+queue::swap
+queue::front
+queue::back
+
+
 #include <iostream>
 #include <stack>
 
