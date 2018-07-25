7f869db7bb55e856870ce63c157a66c776eb7125
taobaup <2970749159@qq.com>
2018-07-25 14:19:11 +0800

    Update MyStack.cpp

--- LeetCode/225. Implement Stack using Queues/MyStack.cpp
+++ LeetCode/225. Implement Stack using Queues/MyStack.cpp
@@ -67,16 +67,6 @@ public:
 
 
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * bool param_4 = obj.empty();
- */
-
-
 
 /**
  * Your MyStack object will be instantiated and called as such:
