7fc06cdd30161e913b5408322d31a9ae8cc11756
taobaup <2970749159@qq.com>
2018-07-26 11:40:21 +0800

    Update firstMissingPositive.cpp

--- LeetCode/041. First Missing Positive/firstMissingPositive.cpp
+++ LeetCode/041. First Missing Positive/firstMissingPositive.cpp
@@ -25,3 +25,29 @@ public:
         return maxNum + 1;
     }
 };
+
+
+
+
+2:
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+    	int len = nums.size();
+        for(int i = 0; i < len; ++i)
+        {
+        	while(nums[i] >= 1 && nums[i] <= len && nums[i] != nums[nums[i] - 1])
+        	{
+        		swap(nums[i], nums[nums[i] - 1]);
+        	}
+        }
+
+        for(int i = 0; i < len; ++i)
+        {
+        	if(nums[i] != i + 1)
+        		return i + 1;
+        }
+
+        return len + 1;
+    }
+};
