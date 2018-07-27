16ff8cd359c78340ea0e8725166948a7949fd847
taobaup <2970749159@qq.com>
2018-07-27 10:40:58 +0800

    Create climbStairs.cpp

---
+++ LeetCode/070. Climbing Stairs/climbStairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev = 0;
+        int cur = 1;
+
+        for(int i = 1; i <= n; ++i)
+        {
+        	int temp = cur;
+        	cur += prev;
+        	prev = temp;
+        }
+
+        return cur;
+    }
+};
